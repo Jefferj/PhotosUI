e194a8bda59da4511517e67a6bb93bbeabd92fe4
Jefferson Naranjo <jefferj080@gmail.com>
2022-06-02 17:43:07 -0500

    Initial Commit

---
+++ PhotosUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotosUI
+//
+//  Created by Apple on 2/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotosUI/PhotosUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosUIApp.swift
+//  PhotosUI
+//
+//  Created by Apple on 2/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotosUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
